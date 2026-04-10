ad1670f557bd40731d2b4bcfd32afd5bee297f40
Mario Villoria <mariovilloria@gmail.com>
2026-04-10 12:31:50 -0500

    base limpia antes de separar js de index

---
+++ backend/static/js/index.js
@@ -0,0 +1,7 @@
+console.log("index.js cargado correctamente");
+
+console.log("🔥 FIREBASE FUNCIONANDO");
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("🌍 ENTORNO ACTIVO:", window.APP_ENV || "no definido");
+});
